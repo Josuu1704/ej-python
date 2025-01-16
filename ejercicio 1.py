49a38593600997ab6136185e0ff0a0f9c2a10bdb
josue <jmzo17.jz@gmail.com>
2025-01-16 12:34:57 +0100

    Ejercicio 1

--- ejercicio 1.py
+++ ejercicio 1.py
@@ -1,1 +1,11 @@
-print("SOY JS")
+vector =[]
+
+for i in range(10):
+    num = float(input(f"Dime un numero {i+1} del vector: "))
+    vector.append(num)
+
+valor_max = max(vector)
+
+repeticion = vector.count(valor_max)
+
+print(f"El valor maximo es: {valor_max} y se repite {repeticion} veces")
